008d6541b9b2adb425943ae94deb6381c05cf5f2
Kateryna Purchel <kateryna@ardanis.com>
2017-11-23 21:09:37 +0000

    first init

---
+++ telegram_bot/server-process_message.py
@@ -0,0 +1,6 @@
+
+def process_message(update):
+    data = {}
+    data["chat_id"] = update["message"]["from"]["id"]
+    data["text"] = "I can hear you!"
+    r = requests.post(get_url("sendMessage"), data=data)
